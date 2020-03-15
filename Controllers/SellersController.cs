1340021d3cbf0009a5184dc43a86091e5531e1f6
wellingtoncgs30 <wellingtoncgs30@gmail.com>
2020-03-15 15:55:21 -0300

    Seller details and eager loading

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -75,5 +75,21 @@ namespace SalesWebMvc.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(uint? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
