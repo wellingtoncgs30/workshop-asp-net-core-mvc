ef6f1237d047bf6da4b8615dd417fc6d2d2759a0
wellingtoncgs30 <wellingtoncgs30@gmail.com>
2020-03-15 15:27:18 -0300

    Delete seller

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -53,91 +53,27 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        // GET: Sellers/Edit/5
-        public async Task<IActionResult> Edit(uint? id)
+        public IActionResult Delete(uint? id)
         {
-            if (id == null)
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
             {
                 return NotFound();
             }
 
-            var seller = await _context.Seller.FindAsync(id);
-            if (seller == null)
-            {
-                return NotFound();
-            }
-            return View(seller);
+            return View(obj);
         }
 
-        // POST: Sellers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(uint id, [Bind("Id,Name,Email,BirthDate,BaseSalary")] Seller seller)
+        public IActionResult Delete(uint id)
         {
-            if (id != seller.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(seller);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SellerExists(seller.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(seller);
-        }
-
-        // GET: Sellers/Delete/5
-        public async Task<IActionResult> Delete(uint? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var seller = await _context.Seller
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (seller == null)
-            {
-                return NotFound();
-            }
-
-            return View(seller);
-        }
-
-        // POST: Sellers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(uint id)
-        {
-            var seller = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(seller);
-            await _context.SaveChangesAsync();
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
-
-        private bool SellerExists(uint id)
-        {
-            return _context.Seller.Any(e => e.Id == id);
-        }
-
-
     }
 }
