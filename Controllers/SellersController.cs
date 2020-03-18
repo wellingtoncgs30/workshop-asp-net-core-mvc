b500446392da3e4df4984cfbb74d1d99d729d91f
wellingtoncgs30 <wellingtoncgs30@gmail.com>
2020-03-18 16:33:54 -0300

    Validation

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -53,6 +53,12 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if(!ModelState.IsValid) 
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -118,6 +124,12 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(uint id, Seller seller)
         {
+            if(!ModelState.IsValid) 
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments}
+                return View(viewModel);
+            }
             if(id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch!"});
--- Models/Seller.cs
+++ Models/Seller.cs
@@ -9,9 +9,14 @@ namespace SalesWebMvc.Models
     public class Seller 
     {
         public uint Id { get;set; }
+        [Required(ErrorMessage = "{0} required!")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1} characters!")]
         public string Name { get;set; }
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required!")]
+        [EmailAddress(ErrorMessage = "Enter a valid email!")]
         public string Email { get;set; }
+        [Required(ErrorMessage = "{0} required!")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
@@ -19,6 +24,8 @@ namespace SalesWebMvc.Models
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get;set; }
+        [Required(ErrorMessage = "{0} required!")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public Department Department { get;set; }
         [Display(Name = "Department")]
         public uint DepartmentId { get; set; }
